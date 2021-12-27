d1d4cc6c73b3e84e6f461f18040836bddc741c26
ArturoArriaga <32577651+ArturoArriaga@users.noreply.github.com>
2021-12-27 09:16:45 -0600

    Initial Commit

---
+++ SwiftUI.Views/SwiftUI_ViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ViewsApp.swift
+//  SwiftUI.Views
+//
+//  Created by Arturo Arriaga on 12/27/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
