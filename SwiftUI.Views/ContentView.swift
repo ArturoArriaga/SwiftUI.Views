d3de5a9e933ba70a1aed79ff20b634335fb0f0fa
ArturoArriaga <32577651+ArturoArriaga@users.noreply.github.com>
2022-01-03 21:22:26 -0600

    Spacers and Geo Reader

--- SwiftUI.Views/ContentView.swift
+++ SwiftUI.Views/ContentView.swift
@@ -111,20 +111,158 @@ struct Lazy_Vstack: View {
     }
 }
 
+struct LazyVstack_WithScrolling: View {
+    let w: CGFloat = 200
+    let h: CGFloat = 150
+    var body: some View {
+        ScrollView {
+            LazyVStack(spacing: 5, pinnedViews: [.sectionHeaders,.sectionFooters]) {
+                Section {
+                    Text("Arturo Arriaga").frame(width: w, height: h).foregroundColor(.white).background(.blue)
+                 Text("Arturo Arriaga").frame(width: w, height: h).foregroundColor(.white).background(.blue)
+                 Text("Arturo Arriaga").frame(width: w, height: h).foregroundColor(.white).background(.blue)
+                 Text("Arturo Arriaga").frame(width: w, height: h).foregroundColor(.white).background(.blue)
+                 Text("Arturo Arriaga").frame(width: w, height: h).foregroundColor(.white).background(.blue)
+                 Text("Arturo Arriaga").frame(width: w, height: h).foregroundColor(.white).background(.blue)
+                 Text("Arturo Arriaga").frame(width: w, height: h).foregroundColor(.white).background(.blue)
+                 Text("Arturo Arriaga").frame(width: w, height: h).foregroundColor(.white).background(.blue)
+                 Text("Arturo Arriaga").frame(width: w, height: h).foregroundColor(.white).background(.blue)
+                } header: {
+                    Text("Header")
+                } footer: {
+                    Text("Footer")
+                }
+
+            }
+        }
+    }
+}
+
+struct LazyHstack_WithScrolling: View {
+    let w: CGFloat = 200
+    let h: CGFloat = 150
+    var body: some View {
+        ScrollView(.horizontal) {
+            LazyHStack(spacing: 5, pinnedViews: [.sectionHeaders,.sectionFooters]) {
+                Section {
+                    Text("Arturo Arriaga").frame(width: w, height: h).foregroundColor(.white).background(.blue)
+                 Text("Arturo Arriaga").frame(width: w, height: h).foregroundColor(.white).background(.blue)
+                 Text("Arturo Arriaga").frame(width: w, height: h).foregroundColor(.white).background(.blue)
+                 Text("Arturo Arriaga").frame(width: w, height: h).foregroundColor(.white).background(.blue)
+                 Text("Arturo Arriaga").frame(width: w, height: h).foregroundColor(.white).background(.blue)
+                 Text("Arturo Arriaga").frame(width: w, height: h).foregroundColor(.white).background(.blue)
+                 Text("Arturo Arriaga").frame(width: w, height: h).foregroundColor(.white).background(.blue)
+                 Text("Arturo Arriaga").frame(width: w, height: h).foregroundColor(.white).background(.blue)
+                 Text("Arturo Arriaga").frame(width: w, height: h).foregroundColor(.white).background(.blue)
+                } header: {
+                    Text("Header")
+                } footer: {
+                    Text("Footer")
+                }
+
+            }
+        }
+    }
+}
+
+struct ZStack_Basic: View {
+    var body: some View {
+        ZStack {
+            Color.blue
+            VStack {
+                Text("Arturo Arriaga").font(.largeTitle)
+                Text("Introduction").foregroundColor(.white)
+                Text("Z Stacks are great for setting a background color.").frame(maxWidth: .infinity).padding().background(Color.cyan)
+            }
+            // Comment out the following line to see what happens to the safe area edges
+        }.ignoresSafeArea()
+    }
+}
+
+struct ZStack_Layering: View {
+    var body: some View {
+        VStack(spacing: 20) {
+            Text("ZStack")
+            // spacer configurations: can have a min limit. Pushes things away.
+//            Spacer(minLength: 10)
+            Spacer().frame(width: 10, height: 30)
+            
+            ZStack(alignment: .bottomTrailing) {
+                Image("harvard.svg")
+                    .resizable()
+                    .scaledToFit()
+                
+                Rectangle()
+                    .fill(Color.white.opacity(0.6))
+                    .frame(maxWidth: .infinity, maxHeight: 100)
+                
+                Text("Software Engineering")
+                    .font(.title)
+                    .padding()
+            }
+        }
+    }
+}
+
+struct Spacer_ex : View {
+    var body: some View {
+        VStack {
+            Text("Spacer").font(.largeTitle)
+            Text("Introduction").foregroundColor(.gray)
+            Text("Spacers push things away either vertically or horizontally").background(.yellow)
+            Image(systemName: "arrow.up.circle.fill")
+            Spacer()
+            Image(systemName: "arrow.down.circle.fill")
+            
+            HStack {
+                Text("Horizontal Spacer")
+                Image(systemName: "arrow.right.circle.fill")
+                Spacer()
+                Image(systemName: "arrow.left.circle.fill")
+            }.padding(.horizontal)
+            Color.yellow.frame(maxHeight: 50)
+        }.font(.title)
+    }
+}
+
+struct GeoReader_Intro: View {
+    var body: some View {
+        VStack(spacing: 20) {
+            Text("Geometry Reader").font(.largeTitle)
+            Text("Introduction").foregroundColor(.gray)
+            Text("The geo reader is a container view that pushes out to fill up all available space. You use it to help with positioning items within it.")
+            
+            GeometryReader { geometryProxy in
+                VStack {
+                    Text("Width \(geometryProxy.size.width)")
+                    Text("Width \(geometryProxy.size.height)")
+
+                }.padding().foregroundColor(.white).font(.title)
+            }.background(Color.pink)
+            
+        }
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
 //        ContentView()
+        
         // add this group container to see the view on multiple devices.
 //        Group {
 //            HeaderView().previewInterfaceOrientation(.landscapeLeft)
-//            HeaderView().preferredColorScheme(.dark).previewInterfaceOrientation(.landscapeRight)
+// HeaderView().preferredColorScheme(.dark).previewInterfaceOrientation(.landscapeRight)
 //            HeaderView().previewDevice(.init(rawValue: "iPad mini (6th generation)"))
-//
 //        }
         
 //        Vstack_Nesting()
-
-        Lazy_Vstack()
+//        Lazy_Vstack()
+//        LazyVstack_WithScrolling()
+//        LazyHstack_WithScrolling()
+//        ZStack_Basic()
+//        ZStack_Layering()
+//        Spacer_ex()
+        GeoReader_Intro()
     }
 }
